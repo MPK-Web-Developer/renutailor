038a6752cafc9d92ef59561e81708e207e60ed1e
Praveen Kumar <m.praveenkumar.github@gmail.com>
2023-12-22 06:29:43 +0530

    Header Created

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-console.log("Working");
+// {------ Start Variables ------}
+
+// {------ End Variables --------}
+
+// {------ Start Header ------}
+window.addEventListener("scroll", function () {
+	let header = document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 0);
+});
+// {------ End Header --------}
