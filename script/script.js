ee95271f73884225e017d06335c0c06247e7d667
Praveen Kumar <m.praveenkumar.github@gmail.com>
2023-12-22 07:49:25 +0530

    Button glass color effect added

--- script/script.js
+++ script/script.js
@@ -8,3 +8,16 @@ window.addEventListener("scroll", function () {
 	header.classList.toggle("sticky", window.scrollY > 0);
 });
 // {------ End Header --------}
+
+// {------ Start Glass Color Effect ------}
+let glassButton = document.querySelectorAll(".button-glass-color-effect");
+glassButton.forEach((singleButton) => {
+	singleButton.onmousemove = function (e) {
+		let x = e.pageX - singleButton.offsetLeft;
+		let y = e.pageY - singleButton.offsetTop;
+
+		singleButton.style.setProperty("--x", x + "px");
+		singleButton.style.setProperty("--y", y + "px");
+	};
+});
+// {------ End Glass Color Effect --------}
