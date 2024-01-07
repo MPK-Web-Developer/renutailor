f9eefb60cdaac636b51e041138bc613bb3a64642
Praveen Kumar <m.praveenkumar.github@gmail.com>
2024-01-07 19:14:53 +0530

    slider responsive updated

--- script/script.js
+++ script/script.js
@@ -5,17 +5,6 @@ const toggleIcon = document.querySelectorAll(".mobile-nav-toggle--icon i");
 // {------ End Variables --------}
 
 // {------ Start Active Page ------}
-/*
-const linkActivePage = window.location.pathname;
-const navigationLinks = document.querySelectorAll(
-	"nav ul.header-navigation li a"
-);
-navigationLinks.forEach((getLink) => {
-	if (getLink.href.includes(`${linkActivePage}`)) {
-		getLink.classList.add("nav-active");
-	}
-});
-*/
 // {------ End Active Page --------}
 
 // {------ Start Nav Bar ------}
@@ -56,11 +45,8 @@ var slide = document.getElementsByClassName("slide"),
 
 leftArrow.onclick = function () {
 	var temp = slide[0].getAttribute("class");
-
 	slide[0].setAttribute("class", slide[1].getAttribute("class"));
-
 	slide[1].setAttribute("class", slide[2].getAttribute("class"));
-
 	slide[2].setAttribute("class", temp);
 };
 
@@ -74,72 +60,6 @@ rightArrow.onclick = function () {
 
 	slide[1].setAttribute("class", temp);
 };
-
-/*
-const sliderContainer = document.querySelector(".slider-container");
-const sliderControlsContainer = document.querySelector(".slider-controls");
-const sliderControls = ["previous", "next"];
-const sliderItems = document.querySelectorAll(".slider-item");
-
-class Carousel {
-	constructor(container, items, controls) {
-		this.carouselContainer = container;
-		this.carouselControls = controls;
-		this.carouselArray = [...items];
-	}
-
-	updateSlider() {
-		this.carouselArray.forEach((el) => {
-			el.classList.remove("slider-item-1");
-			el.classList.remove("slider-item-2");
-			el.classList.remove("slider-item-3");
-			el.classList.remove("slider-item-4");
-			el.classList.remove("slider-item-5");
-		});
-
-		this.carouselArray.slice(0, 5).forEach((el, i) => {
-			el.classList.add(`slider-item-${i + 1}`);
-		});
-	}
-
-	setCurrentState(direction) {
-		if (direction.className == "slider-controls-previous") {
-			this.carouselArray.unshift(this.carouselArray.pop());
-		} else {
-			this.carouselArray.push(this.carouselArray.shift());
-		}
-		this.updateSlider();
-	}
-
-	setControls() {
-		this.carouselControls.forEach((control) => {
-			sliderControlsContainer.appendChild(
-				document.createElement("button")
-			).className = `slider-controls-${control}`;
-			document.querySelector(`slider-controls-${control}`).innerHTML = control;
-		});
-	}
-
-	useControls() {
-		const triggers = [...sliderControlsContainer.childNodes];
-		triggers.forEach((control) => {
-			control.addEventListener("click", (e) => {
-				e.preventDefault();
-				this.setCurrentState(control);
-			});
-		});
-	}
-}
-
-const creativeSlider = new Carousel(
-	sliderContainer,
-	sliderItems,
-	sliderControls
-);
-
-creativeSlider.setControls();
-creativeSlider.useControls();
-*/
 // {------ End Slider --------}
 
 // {------ Start Glass Color Effect ------}
